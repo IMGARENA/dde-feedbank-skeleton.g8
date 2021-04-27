a1ce9f49a473d3bd51431adaa681e24066de60ff
William Lea <william.lea@imgarena.com>
2021-04-27 16:44:23 +0100

    update resolver

--- src/main/g8/project/Resolver.scala
+++ src/main/g8/project/Resolver.scala
@@ -5,7 +5,7 @@ object Resolver extends DependencyBuilders {
   val s3Prefix = "dde-artifacts/jars"
 
   val ddeResolvers = Seq(
-    "DDE snapshots" at s"s3://s3-eu-west-1.amazonaws.com/$s3Prefix/snapshots",
-    "DDE releases" at s"s3://s3-eu-west-1.amazonaws.com/$s3Prefix/releases"
+    "DDE snapshots" at s"s3://s3-eu-west-1.amazonaws.com/\$s3Prefix/snapshots",
+    "DDE releases" at s"s3://s3-eu-west-1.amazonaws.com/\$s3Prefix/releases"
   )
 }
